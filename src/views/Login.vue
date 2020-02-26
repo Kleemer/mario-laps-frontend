8718129829fe5bd50bf6b2b48e3a289a0b0cf650
Yassine Ammar <yassine@whispli.com>
2020-02-26 18:48:51 +1100

    feat: skip login if cookie exists

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -6,8 +6,8 @@
     <VCardText class="text-center">
       <UsernameSelector v-model="username"/>
       <VBtn
-        :disabled="pending"
-        :loading="pending"
+        :disabled="isPending"
+        :loading="isPending"
         color="info"
         depressed
         x-large
@@ -24,6 +24,7 @@ import UsernameSelector from '@/components/UsernameSelector.vue'
 
 import { getRandomString } from '@/shared/string.js'
 import { getPlayerUsername } from '@/shared/user.js'
+import { getLoggedInCookie } from '@/shared/auth'
 
 import { login, logout } from '@/api/types/routes/auth'
 import { createMarioLap } from '@/api/types/routes/mario-lap'
@@ -35,31 +36,38 @@ export default {
     UsernameSelector,
   },
   data: () => ({
-    pending: false,
+    isPending: false,
     username: null,
   }),
   mounted() {
     this.$store.dispatch('reset')
     this.username = getPlayerUsername()
+    if (getLoggedInCookie()) {
+      this.goToNext()
+      return
+    }
   },
   methods: {
     async onSubmit() {
       try {
-        this.pending = true
+        this.isPending = true
         await login({ username: this.username })
         this.$store.dispatch('setPlayerUsername', this.username)
-        if (this.$route.params.nextUrl) {
-          this.$router.push(this.$route.params.nextUrl)
-        } else {
-          this.$router.push('home')
-        }
+        this.goToNext()
       } catch (err) {
         // @todo add snackbar
         console.log('Something went wrong')
       } finally {
-        this.pending = false
+        this.isPending = false
       }
     },
+    goToNext() {
+      if (this.$route.params.nextUrl) {
+        this.$router.push(this.$route.params.nextUrl)
+      } else {
+        this.$router.push('home')
+      }
+    }
   },
 }
 </script>

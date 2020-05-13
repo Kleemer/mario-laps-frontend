8632fedcaa46434c5d05e22666822a9ec0deb77f
Yassine Ammar <yassine@whispli.com>
2020-05-13 20:56:16 +1000

    fix: update socket url for prod

--- src/plugins/socket-io.ts
+++ src/plugins/socket-io.ts
@@ -2,7 +2,7 @@ import { VueConstructor } from 'vue'
 import VueSocketIOExt from 'vue-socket.io-extended'
 import io from 'socket.io-client'
 
-const socket = io('http://localhost:5000')
+const socket = io(process.env.VUE_APP_SOCKET_URL)
 
 export default (Vue: VueConstructor): void => {
     Vue.use(VueSocketIOExt, socket)

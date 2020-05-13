2c59f59c39b087671a675a8cfe1d4e38452f06f8
Yassine Ammar <yassine@whispli.com>
2020-05-13 23:34:05 +1000

    fix: toPath on non prod environments

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -2,7 +2,7 @@ import { REST_API_HOST } from './constants'
 
 export const toPath = (path?: string | null): string => {
   let apiUrl = '/api'
-  if (process.env.NODE_ENV) {
+  if (process.env.NODE_ENV === 'production') {
     apiUrl = REST_API_HOST
   }
 

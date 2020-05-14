4fd0965f900d27d9a21819c8c57d2860b44d12a6
Yassine Ammar <yassine@whispli.com>
2020-05-14 23:59:11 +1000

    feat: static.json proxy

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -1,10 +1,5 @@
 import { REST_API_HOST } from './constants'
 
 export const toPath = (path?: string | null): string => {
-  let apiUrl = '/api'
-  if (process.env.NODE_ENV === 'production') {
-    apiUrl = REST_API_HOST
-  }
-
-  return apiUrl + (path ? path.replace(/^(\w)/, '/$1') : '')
+  return '/api' + (path ? path.replace(/^(\w)/, '/$1') : '')
 }
